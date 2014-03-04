b6588507974375f2c7cee1994cb2430f35984f32
Gileno Oliveira <gileno@gmail.com>
2014-03-04 16:40:55 -0300

    Refactoring module and custom filter

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
-(function () {
-    'use strict';
-    angular.module('app', ['ngRoute', 'ngAnimate'])
-            .config(function ($routeProvider, $locationProvider) {
-                $routeProvider.when('/cart', {
-                    templateUrl: 'app/cart/cart.html'
-                });
-                $routeProvider.when('/', {
-                    templateUrl: 'app/home/home.html',
-                    controller: 'homeController as home'
-                });
-                $locationProvider.html5Mode(true);
+'use strict';
+
+angular.module('app', ['ngRoute', 'ngAnimate'])
+        .config(function ($routeProvider, $locationProvider) {
+            $routeProvider.when('/cart', {
+                templateUrl: 'app/cart/cart.html'
             });
-}());
+            $routeProvider.when('/', {
+                templateUrl: 'app/home/home.html',
+                controller: 'homeController as home'
+            });
+            $locationProvider.html5Mode(true);
+        });
+
--- app/cart/cartSummary.js
+++ app/cart/cartSummary.js
@@ -4,14 +4,15 @@
     function cartSummary() {
         return {
             restrict: 'E',
-            template: '<a class="btn btn-lg btn-primary" href="/cart">' +
-                '<i class="glyphicon glyphicon-shopping-cart"></i>' +
-                'Items: {{ numberOfItems() }} | Total: {{ total() | currency }}' +
+            template:
+                '<a class="btn btn-lg btn-primary" href="/cart">' +
+                    '<i class="glyphicon glyphicon-shopping-cart"></i>' +
+                    'Items: {{ numberOfItems }} | Total: {{ total | currency }}' +
                 '</a>',
             replace: true,
             scope: {
-                numberOfItems: "&",
-                total: "&"
+                numberOfItems: "@",
+                total: "@"
             }
         };
     }
